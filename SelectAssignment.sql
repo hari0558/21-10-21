f4ecc6e834a05d8972aea625d77d087ddaa23b6c
hari0558 <89850252+hari0558@users.noreply.github.com>
2021-10-23 11:22:28 +0530

    Add files via upload

---
+++ SelectAssignment.sql
@@ -0,0 +1,12 @@
+use hr_schema;
+select * from employees;
+select * from employees where department_id=10;
+select * from employees where salary>10000 and commission_pct>0.02;
+select * from employees where department_id in (10,20,50);
+select * from employees where first_name like 'S%' and first_name like '%n';
+select * from employees where first_name or last_name like'%oc';
+select * from employees where commission_pct is null;
+select * from employees order by manager_id;
+select * from employees order by salary desc,salary asc;
+select * from employees where department_id=50 and salary>5000;
+select * from employees where job_id='SA_REP' or job_id='SA_MAN';
